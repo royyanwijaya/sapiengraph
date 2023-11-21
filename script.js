fcbbfd66879742fc017656b2ca215123d9270e14
Roy <62423290+royyanwijaya@users.noreply.github.com>
2023-11-21 17:14:23 +0700

    updates

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleSwitch = document.getElementById('billingSwitch');
+    const contentToHideElements = document.querySelectorAll('.contentToHide');
+
+    // Initial state check
+    contentToHideElements.forEach(function(element) {
+      element.style.display = toggleSwitch.checked ? 'inline' : 'none';
+    });
+
+    // Add event listener for toggle switch change
+    toggleSwitch.addEventListener('change', function () {
+      contentToHideElements.forEach(function(element) {
+        element.style.display = toggleSwitch.checked ? 'inline' : 'none';
+      });
+    });
+  });
