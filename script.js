39b555806a3b609dcaef29c9c8017b59ad6bfa7d
Roy <62423290+royyanwijaya@users.noreply.github.com>
2023-11-27 17:31:20 +0700

    updates

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const toggleSwitch = document.getElementById('billingSwitch');
-    const contentToHideElements = document.querySelectorAll('.contentToHide');
+  const toggleSwitch = document.getElementById('billingSwitch');
+  const contentToHideElements = document.querySelectorAll('.contentToHide');
+  const billedElements = document.querySelectorAll('.billed');
 
-    // Initial state check
-    contentToHideElements.forEach(function(element) {
+  // Initial state check
+  contentToHideElements.forEach(function (element) {
+    element.style.display = toggleSwitch.checked ? 'inline' : 'none';
+  });
+
+  // Set the initial text based on the toggle switch state for all billed elements
+  billedElements.forEach(function (element) {
+    element.textContent = toggleSwitch.checked ? '/mo, billed annually' : '/mo, billed monthly';
+  });
+
+  // Add event listener for toggle switch change
+  toggleSwitch.addEventListener('change', function () {
+    contentToHideElements.forEach(function (element) {
       element.style.display = toggleSwitch.checked ? 'inline' : 'none';
     });
 
-    // Add event listener for toggle switch change
-    toggleSwitch.addEventListener('change', function () {
-      contentToHideElements.forEach(function(element) {
-        element.style.display = toggleSwitch.checked ? 'inline' : 'none';
-      });
+    // Update the text based on the toggle switch state for all billed elements
+    billedElements.forEach(function (element) {
+      element.textContent = toggleSwitch.checked ? '/mo, billed annually' : '/mo, billed monthly';
     });
+  });
 
-    
   // Tooltip Code  
   $('[data-toggle="tooltip"]').tooltip();
-  
-  });
+});
