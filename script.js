fe91dbb8fa2731f8dea5e22f246d39853341c385
Roy <62423290+royyanwijaya@users.noreply.github.com>
2023-11-28 16:29:29 +0700

    updatez

--- script.js
+++ script.js
@@ -1,30 +1,44 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
+
   const toggleSwitch = document.getElementById('billingSwitch');
-  const contentToHideElements = document.querySelectorAll('.contentToHide');
   const billedElements = document.querySelectorAll('.billed');
 
-  // Initial state check
-  contentToHideElements.forEach(function (element) {
-    element.style.display = toggleSwitch.checked ? 'inline' : 'none';
-  });
-
-  // Set the initial text based on the toggle switch state for all billed elements
-  billedElements.forEach(function (element) {
+  // Get all contentToHide elements 
+  const contentToHide = document.querySelectorAll('.contentToHide');
+   
+  // Initial billed text
+  billedElements.forEach(function(element) {
     element.textContent = toggleSwitch.checked ? '/mo, billed annually' : '/mo, billed monthly';
   });
 
-  // Add event listener for toggle switch change
-  toggleSwitch.addEventListener('change', function () {
-    contentToHideElements.forEach(function (element) {
-      element.style.display = toggleSwitch.checked ? 'inline' : 'none';
-    });
-
-    // Update the text based on the toggle switch state for all billed elements
-    billedElements.forEach(function (element) {
+  // Toggle change listener
+  toggleSwitch.addEventListener('change', function() {
+    
+    // Update billed text
+    billedElements.forEach(function(element) {
       element.textContent = toggleSwitch.checked ? '/mo, billed annually' : '/mo, billed monthly';
     });
+
+    // Loop through each contentToHide 
+    contentToHide.forEach(function(element) {
+
+      const delText = element.querySelector('del');
+      const spanText = element.querySelector('span');
+
+      if(!toggleSwitch.checked) {
+        element.classList.add('text-white');
+        delText.classList.remove('text-muted'); 
+        spanText.classList.remove('text-info');
+      } else {
+        element.classList.remove('text-white');     
+        delText.classList.add('text-muted');
+        spanText.classList.add('text-info');
+      }
+
+    });
+
   });
 
-  // Tooltip Code  
   $('[data-toggle="tooltip"]').tooltip();
+
 });
